f686871a38660732de40dd5093c5183dc9ee1784
TemiGraceO <graceolorunfemi96@gmail.com>
2025-11-20 12:55:39 +0100

    corrected

--- paravision-app/src/SplashPage.js
+++ paravision-app/src/SplashPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import './first_one.css';
-import './ParticlesBackground';
+
 
 function SplashPage() {
   useEffect(() => {
@@ -54,7 +54,7 @@ function SplashPage() {
       <div className='first'>
         <div className='up'>
           <img src='logo2.png' alt='logo' style={{ width: '3%',marginLeft:'3%'}} />
-          <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className='toggle'>
   <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
 </svg></div>
                   </div><hr/>
